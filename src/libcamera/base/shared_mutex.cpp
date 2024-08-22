2becb0f0a992b8d95bcc727820d2c4e7c31ecf18
Andrei Agnew 510471659 <aagn5279@uni.sydney.edu.au>
2024-08-22 19:27:27 +1000

    created the shared_mutex.h and shared_mutex.cpp files

---
+++ src/libcamera/base/shared_mutex.cpp
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2021, Google Inc.
+ *
+ * Mutex classes with clang thread safety annotation
+ */
+
+#include <libcamera/base/shared_mutex.h>
+
+/**
+ * \file base/shared_mutex.h
+ * \brief Shared Mutex classes with clang thread safety annotation to allow for concurrent read
+ */
